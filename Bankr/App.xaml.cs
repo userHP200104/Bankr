bccc4195d683198113a33e824ba1e14cd3b79444
userHP200104 <71634994+userHP200104@users.noreply.github.com>
2022-07-26 15:39:38 +0200

    Initial Commit: File Upload

---
+++ Bankr/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Bankr;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Bankr/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bankr;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+

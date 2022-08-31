8da88d45ccecb4eb4cc3d8dd22ca2e6df190a1b3
GordonRodseth <200055@virtualwindow.co.za>
2022-08-31 11:04:22 +0200

    minor front end adjustments

--- Bankr/Views/StaffDetailView.xaml.cs
+++ Bankr/Views/StaffDetailView.xaml.cs
@@ -29,7 +29,7 @@ public partial class StaffDetailView : ContentPage
        
         Name.Placeholder=StaffMember.Name;
         Surname.Placeholder=StaffMember.Surname;
-        Role.Placeholder=StaffMember.Role;
+        Role.Title=StaffMember.Role;
     }
     private async void OnEditClicked(object sender, EventArgs e)
     {
@@ -38,8 +38,8 @@ public partial class StaffDetailView : ContentPage
         int staffid = int.Parse((string)StaffId);
 
         string staffname = Name.Text; 
-        string staffrole=Surname.Text;
-        string staffsurname=Role.Text;
+        string staffsurname=Surname.Text;
+        string staffrole=Role.SelectedItem.ToString();
 
         App.StaffRepo.UpdateStaff(staffid, staffname, staffsurname, staffrole);
         await Navigation.PopAsync();
